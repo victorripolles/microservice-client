beae77bd71e55e6f57e22c971ca8291d403eda1d
vripolles <victor.ripolles.garcia@es.ey.com>
2025-09-09 12:53:32 +0200

    chore(setup): initial Spring Boot project structure

---
+++ src/test/java/com/victorripolles/microservice_client/MicroserviceClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorripolles.microservice_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
